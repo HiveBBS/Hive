503542e23611975eaefa2c2fa2e44f4ae05a66ae
chattray <chea0073@gmail.com>
2021-11-15 11:02:04 -0800

    api/user routes. adds index.js for models. configured server.js for route handling

---
+++ src/routes/api-routes.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express');
+const { posting } = require('../models/posting.js');
+
+const router = express.Router();
+
+router.get('/posting', handleGetPostings);
+router.get('/posting/:id', handleGetOnePosting);
+
+async function handleGetPostings(req, res) {
+  try {
+    let getAllPosts = await posting.findAll();
+    res.status(200).json(getAllPosts);
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+async function handleGetOnePosting(req, res) {
+  try {
+    const id = req.params.id;
+    let getOnePost = await posting.findByPk(id);
+    res.status(200).json({
+      id: getOnePost.id,
+    });
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,23 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan')
+
+const v1Routes = require ('./routes/api-routes.js')
+
 const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ 
+  extended: true
+}))
+
+app.use('/api/v1', v1Routes);
+
 
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to HiveBBS');
