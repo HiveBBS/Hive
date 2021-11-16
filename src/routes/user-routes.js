f2d1b4f2ee483703bbedd9cdf154201a3c40b46c
Tripp Lucas <tripppdx@gmail.com>
2021-11-16 15:39:54 -0800

    genericize user routes

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -7,23 +7,34 @@ const { posting } = require("../models/index.js");
 const bearerHandler = require("../auth/bearer.js");
 const aclHandler = require("../auth/acl.js");
 const socketService = require("../socket.js");
+const dataModules = require("../models");
 
 const router = express.Router();
 
+router.param("model", (req, res, next) => {
+  const modelName = req.params.model;
+  if (dataModules[modelName]) {
+    req.model = dataModules[modelName];
+    next();
+  } else {
+    next("Invalid Model");
+  }
+});
+
 router.post(
-  "/posting",
+  "/:model",
   bearerHandler,
   aclHandler("create"),
   handleCreatePosting
 );
 router.put(
-  "/posting/:id",
+  "/:model/:id",
   bearerHandler,
   aclHandler("update"),
   handleUpdatePosting
 );
 router.delete(
-  "/posting/:id",
+  "/:model/:id",
   bearerHandler,
   aclHandler("delete"),
   handleDeletePosting
@@ -32,9 +43,10 @@ router.delete(
 async function handleCreatePosting(req, res) {
   try {
     let obj = req.body;
-    console.log("****************", req.user.dataValues.username);
+    console.log("****************", req.model);
+
     let sellerObj = { seller: `${req.user.dataValues.username}`, ...obj };
-    let newPosting = await posting.create(sellerObj);
+    let newPosting = await req.model.create(sellerObj);
     socketService("update", {
       message: "New item for sale",
       ...newPosting.dataValues,
@@ -50,7 +62,7 @@ async function handleUpdatePosting(req, res) {
     const id = req.params.id;
     const obj = req.body;
     console.log("**********************", id, obj);
-    let updatedPosting = await posting.update(obj, { where: { id } });
+    let updatedPosting = await req.model.update(obj, { where: { id } });
     res.status(200).json(updatedPosting);
   } catch (err) {
     console.error(err);
@@ -60,7 +72,7 @@ async function handleUpdatePosting(req, res) {
 async function handleDeletePosting(req, res) {
   try {
     let id = req.params.id;
-    let deletedPosting = await posting.destroy({ where: { id } });
+    let deletedPosting = await req.model.destroy({ where: { id } });
     res.status(200).json(deletedPosting);
   } catch (err) {
     console.error(err);
