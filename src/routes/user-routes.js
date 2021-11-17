94f9cde5578f1f0b7f28ae42e73c871759080733
stefrie <sriehle@gmail.com>
2021-11-17 09:42:22 -0800

    update reference in user-routes

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -9,86 +9,87 @@ const dataModules = require("../models");
 const router = express.Router();
 
 router.param("model", (req, res, next) => {
-  const modelName = req.params.model;
-  if (dataModules[modelName]) {
-    req.model = dataModules[modelName];
-    next();
-  } else {
-    next("Invalid Model");
-  }
+	const modelName = req.params.model;
+	if (dataModules[modelName]) {
+		req.model = dataModules[modelName];
+		next();
+	} else {
+		next("Invalid Model");
+	}
 });
 
 router.post(
-  "/:model",
-  bearerHandler,
-  aclHandler("create"),
-  handleCreatePosting
+	"/:model",
+	bearerHandler,
+	aclHandler("create"),
+	handleCreatePosting
 );
 router.put(
-  "/:model/:id",
-  bearerHandler,
-  aclHandler("update"),
-  handleUpdatePosting
+	"/:model/:id",
+	bearerHandler,
+	aclHandler("update"),
+	handleUpdatePosting
 );
 router.delete(
-  "/:model/:id",
-  bearerHandler,
-  aclHandler("delete"),
-  handleDeletePosting
+	"/:model/:id",
+	bearerHandler,
+	aclHandler("delete"),
+	handleDeletePosting
 );
 
 async function handleCreatePosting(req, res) {
-  try {
-    let obj = req.body;
+	try {
+		let obj = req.body;
 
-    let sellerObj = { seller: `${req.user.dataValues.username}`, ...obj };
-    let newPosting = await req.model.create(sellerObj);
-    socketService("update", {
-      message: "New item for sale",
-      ...newPosting.dataValues,
-    });
-    res.status(201).json(newPosting);
-  } catch (err) {
-    console.error(err);
-  }
+		let sellerObj = { seller: `${req.user.dataValues.username}`, ...obj };
+		let newPosting = await req.model.create(sellerObj);
+		socketService("update", {
+			message: "New item for sale",
+			...newPosting.dataValues,
+		});
+		res.status(201).json(newPosting);
+	} catch (err) {
+		console.error(err);
+	}
 }
 
 async function handleUpdatePosting(req, res) {
-  try {
-    const id = req.params.id;
-    const obj = req.body;
+	try {
+		const id = req.params.id;
+		const obj = req.body;
 
-    let updator = req.user.dataValues.username;
-    let oldPost = await req.model.findOne({ where: { id } });
-    let poster = oldPost.dataValues.seller;
+		let updator = req.user.dataValues.username;
+		let oldPost = await req.model.model.findOne({ where: { id } });
+		let poster = oldPost.dataValues.seller;
 
-    if (updator === poster) {
-      let updatedPosting = await req.model.update(obj, { where: { id } });
-      res.status(200).json(updatedPosting);
-    } else {
-      res.status(403).send("Invalid user");
-    }
-  } catch (err) {
-    console.error(err);
-  }
+		if (updator === poster) {
+			let updatedPosting = await req.model.model.update(obj, { where: { id } });
+			res.status(200).json(updatedPosting);
+		} else {
+			res.status(403).send("Invalid user");
+		}
+	} catch (err) {
+		console.error(err);
+	}
 }
 
 async function handleDeletePosting(req, res) {
-  try {
-    let id = req.params.id;
+	try {
+		let id = req.params.id;
 
-    let deleter = req.user.dataValues.username;
-    let oldPost = await req.model.findOne({ where: { id } });
-    let poster = oldPost.dataValues.seller;
-    if (deleter === poster) {
-      let deletedPosting = await req.model.destroy({ where: { id } });
-      res.status(200).json(deletedPosting);
-    } else {
-      res.status(403).send("Invalid user");
-    }
-  } catch (err) {
-    console.error(err);
-  }
+		let deleter = req.user.dataValues.username;
+		console.log(req.model);
+		let oldPost = await req.model.model.findOne({ where: { id } });
+		let poster = oldPost.dataValues.seller;
+		if (deleter === poster) {
+			let deletedPosting = await req.model.model.destroy({ where: { id } });
+			res.status(200).json(deletedPosting);
+		} else {
+			res.status(403).send("Invalid user");
+		}
+	} catch (err) {
+		console.error(err);
+	}
 }
 
 module.exports = router;
