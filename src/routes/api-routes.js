2d4f6d1a6eadb30440eeaa52cb7c991a0495d950
chattray <chea0073@gmail.com>
2021-11-16 17:40:24 -0800

    adds partitioned database objects

--- src/routes/api-routes.js
+++ src/routes/api-routes.js
@@ -1,16 +1,27 @@
 'use strict';
 
 const express = require('express');
-const { posting } = require('../models');
+const dataModules = require("../models");
 
 const router = express.Router();
 
-router.get('/posting', handleGetPostings);
-router.get('/posting/:id', handleGetOnePosting);
+router.param("model", (req, res, next) => {
+  const modelName = req.params.model;
+  if (dataModules[modelName]) {
+    req.model = dataModules[modelName];
+    next();
+  } else {
+    next("Invalid Model");
+  }
+});
+
+router.get('/:model', handleGetPostings);
+router.get('/:model/:id', handleGetOnePosting);
 
 async function handleGetPostings(req, res) {
   try {
-    let getAllPosts = await posting.findAll();
+    console.log('**************', req.model)
+    let getAllPosts = await req.model.model.findAll();
     res.status(200).json(getAllPosts);
   } catch (err) {
     console.error(err)
@@ -20,10 +31,10 @@ async function handleGetPostings(req, res) {
 async function handleGetOnePosting(req, res) {
   try {
     const id = req.params.id;
-    let getOnePost = await posting.findByPk(id);
-    res.status(200).json({
-      id: getOnePost.id,
-    });
+    let getOnePost = await req.model.model.findOne({
+      where:{id}
+     });
+    res.status(200).json(getOnePost);
   } catch (err) {
     console.error(err)
   }
